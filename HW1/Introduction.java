558e18acae6eb6d9c1379e896547f9ea4b1f81f1
minhanhltnc <nguyenminhanh1905nt@gmail.com>
2025-09-22 10:16:32 +0700

    my commit

---
+++ HW1/Introduction.java
@@ -0,0 +1,13 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyễn Minh Anh 24021370 K69I-CS5 " +
+                "https://github.com/minhanhltnc/OOP "+
+                 "24021370@vnu.edu.vn");
+        //.out.println("https://github.com/minhanhltnc/OOP");
+        for(int i=9;i>=1;i--)
+        {
+            System.out.println(i+" bottles of beer on the wall, 9 bottles of beer");
+            System.out.println("Take one down, pass it around,");
+        }
+    }
+}
